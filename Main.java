461de52fc90777a314f96128c473f79d34063e3f
pajid9763 <pajid9763@wrdsb.ca>
2020-10-01 16:20:43 +0000

    finished program that calculates largest integer

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * program to calculate largest integer
+ * @author Darian
  */
 public class Main {
 
@@ -9,7 +10,25 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+  Scanner input = new Scanner(System.in);
+
+  // ask the user to input three integers
+  System.out.println("Please enter three integers on separate lines");
+  int num1 = input.nextInt();
+  int num2 = input.nextInt();
+  int num3 = input.nextInt();
+
+  if (num1 >= num2) {
+    if (num1 >= num3)
+      System.out.println("Maximum: " + num1);
+    else
+      System.out.println("Maximum: " + num3);
+
+  } else {
+    if (num2 >= num3)
+      System.out.println("Maximum: " + num2);
+    else 
+      System.out.println("Maximum: " + num3);
+  }
   }
 }
